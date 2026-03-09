b213f4de3aa2f2ca6692ba24752e531352d6b42d
TheGauravsahu <itsgauravsahu7@gmail.com>
2026-03-09 22:48:30 +0530

    [FE] change : from suspense query to normal use query

--- frontend/src/modules/interview/hooks/useInterview.js
+++ frontend/src/modules/interview/hooks/useInterview.js
@@ -1,8 +1,8 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { interviewApi } from "../services/interview.api";
 
 export const useInterview = (id) =>
-  useSuspenseQuery({
+  useQuery({
     queryKey: ["interview", id],
     queryFn: () => interviewApi.getInterview(id),
     staleTime: Infinity,
